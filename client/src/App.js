3ec96092003156bb5daf583d2915cbbd392755b3
Dimitar <dimitarivanov2989@gmail.com>
2020-09-08 14:43:34 +0300

    Login and Register component, set up a redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,31 @@
 import React, {Fragment} from 'react';
 import Navbar from './components/layout/Navbar'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Landing from './components/layout/Landing'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 import './App.css';
+//store related stuff
+import {Provider} from 'react-redux'
+import store from './store'
 
 const App = () => (
-  <Fragment>
+  <Provider store={store}>
+    <Router>
+    <Fragment>
     <Navbar />
-    <Landing />
-  </Fragment>
+    <Route exact path="/" component={Landing}/>
+    <section className='container'>
+      <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </section>
+    </Fragment>
+    </Router>
+  </Provider>
+  
+  
 )
   
 
